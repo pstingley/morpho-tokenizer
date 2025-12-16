00c339387142855016e94b3ab5d940d8756b3558
Patrick Stingley <covertchannel@yahoo.com>
2025-12-16 16:31:11 -0500

    Fix MorphoTokenizer lexicon initialization

--- src/morpho_tokenizer/morpho_vtb_tokenizer.py
+++ src/morpho_tokenizer/morpho_vtb_tokenizer.py
@@ -1,3 +1,13 @@
+
+from importlib import resources
+from pathlib import Path
+
+def _default_lexicon_paths():
+    data_dir = resources.files("morpho_tokenizer") / "data"
+    root = data_dir / "morph_lexicon_by_root.json.gz"
+    proto = data_dir / "morph_lexicon_by_protoroot.json.gz"
+    return str(root), str(proto)
+
 # MorphoTokenizer implementation goes here
 
 class MorphoTokenizer:
